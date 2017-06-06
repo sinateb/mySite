cd0bde4d811e4b16d0a25029cf316999a2a97c37
sina tebiani <sina.tebiany@gmail.com>
2017-06-06 19:55:22 +0200

    34

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mongo=require('mongodb');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,4 +11,24 @@ app.use(express.static(__dirname));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+
+var url='mongodb://localhost/mydb';
+
+mongo.connect(url, function(err,db){
+    if(err) throw err;
+    console.log("Connected to DB") 
+    db.collection('myCollection', function (err, myCollection) {
+        myCollection.find().toArray(function(err, items) {
+            if(err) throw err;
+            app.get('/',function(req,res){
+                res.send(items[0]);
+             }); 
+            console.log(items);          
+        });
+        
+    });
+
+    db.close();
 });
