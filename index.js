ceae76bacac6e53a16a520c6178b40c7100ceb9b
sina tebiani <sina.tebiany@gmail.com>
2017-06-06 17:53:16 +0200

    23

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('Hello World');
+  response.render(__dirname + '/index.html');
 });
 
 app.listen(app.get('port'), function() {
