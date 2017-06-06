0ca3eb184c16c79dac962323476e7f93c3b35b93
sina tebiani <sina.tebiany@gmail.com>
2017-06-06 23:10:26 +0200

    37

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(app.get('port'), function() {
 });
 
 
-var url="mongodb://sinateb:269578@cluster0-shard-00-00-w5m3k.mongodb.net:27017,cluster0-shard-00-01-w5m3k.mongodb.net:27017,cluster0-shard-00-02-w5m3k.mongodb.net:27017/mydb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+var url='mongodb://sinateb:269578@cluster0-shard-00-00-w5m3k.mongodb.net:27017,cluster0-shard-00-01-w5m3k.mongodb.net:27017,cluster0-shard-00-02-w5m3k.mongodb.net:27017/mydb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
 /*mongo.connect(url, function(err,db){
     if(err) throw err;
