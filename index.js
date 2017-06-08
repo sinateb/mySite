ef62dae2caa121469bee539cc565e058e53784cb
sina tebiani <sina.tebiany@gmail.com>
2017-06-08 18:52:37 +0200

    47

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var mongo=require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var formidable = require('formidable');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var cookieParser = require('cookie-parser');
 
 
 app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/home.html');
-});
 app.use(express.static(__dirname));
+app.use(express.static( __dirname + '/userShortner'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-app.use(bodyParser.json());
-app.post('/', function(req, res){
-    console.log(req.body.fname);
-    console.log(req.body.email);
-});
-
 
 var url='mongodb://sinateb:269578@cluster0-shard-00-00-w5m3k.mongodb.net:27017,cluster0-shard-00-01-w5m3k.mongodb.net:27017,cluster0-shard-00-02-w5m3k.mongodb.net:27017/mydb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*app.post('/', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  var fname=req.body.fname;
+  var lname=req.body.lname;
+  res.sendFile(__dirname + '/home.html');
+  mongo.connect(url, function(err,db){
+    if(err) throw err;
+    console.log("Connected to DB") ;
+  db.collection('myCollection').insert({name:fname,lname:lname},function(err,res){
+          if (err) throw err;
+          console.log("1 record inserted");  
+          db.close();
+        });
+  });
+});*/
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/home.html');
+});
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/userShortner/shortner.html');
+});
+
+app.post('/',urlencodedParser,function(req,res){
+    if(!req.body) return res.sendStatus(400);
+    var Username=req.body.Username;
+    mongo.connect(url, function(err,db){
+    if(err) throw err;
+    console.log("Connected to DB") ;
+  db.collection('myCollection').findOne({Username:Username},{Username:Username});
+          
+          res.redirect('/userShortner/shortner.html');
+          console.log("1 record found"); 
+          db.close();
+  });
+});
+
+app.post('/userShortner/shortner.html',urlencodedParser,function(req,res){
+  if(!req.body) return res.sendStatus(400);
+    res.redirect('..')
+});
 
 /*mongo.connect(url, function(err,db){
     if(err) throw err;
@@ -51,5 +93,3 @@ var url='mongodb://sinateb:269578@cluster0-shard-00-00-w5m3k.mongodb.net:27017,c
           db.close();
         });
   });*/
-
-  
